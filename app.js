f5b9af1f04797b0c74cbee7ecf4f46f84e0d0bee
Joel Detweiler <joel.detweiler@gmail.com>
2020-04-10 16:58:49 -0700

    Ensure login works on psql v9

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const cors = require('cors');
-const toNumber = require('lodash/toNumber')
+const cors = require("cors");
+const toNumber = require("lodash/toNumber");
 
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(cors());
 app.use(bodyParser.json());
 const port = process.env.API_PORT || 3000;
@@ -11,21 +11,32 @@ const port = process.env.API_PORT || 3000;
 const { createView, getGame, getGames } = require("./server/api/games");
 const { getReviews } = require("./server/api/reviews");
 const { getUsers } = require("./server/api/users");
-const { getReviewFlags, flagReview, getReviewerFlags, flagReviewer, deleteReviewFlag, deleteReviewerFlag } = require("./server/api/flags");
+const {
+  getReviewFlags,
+  flagReview,
+  getReviewerFlags,
+  flagReviewer,
+  deleteReviewFlag,
+  deleteReviewerFlag
+} = require("./server/api/flags");
 
 app.use(express.static("build"));
 
 const allowedUsersPromise = getUsers();
 
 app.use(async (req, res, next) => {
-  const allowedUsers = await allowedUsersPromise;
-  const userName = req.headers.authorization;
-  const matchingUser = allowedUsers.find(u => u.name === userName)
-  if (matchingUser) {
-    req.user = matchingUser.id;
-    next();
-  } else {
-    res.sendStatus(401);
+  try {
+    const allowedUsers = await allowedUsersPromise;
+    const userName = req.headers.authorization;
+    const matchingUser = allowedUsers.find(u => u.name === userName);
+    if (matchingUser) {
+      req.user = matchingUser.id;
+      next();
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (e) {
+    res.sendStatus(500);
   }
 });
 
@@ -33,7 +44,7 @@ app.get("/api/ping", async (req, res, next) => {
   if (!req.user) {
     res.sendStatus(401);
   } else {
-    await createView(req.user);
+    createView(req.user);
     res.sendStatus(200);
   }
 });
@@ -42,99 +53,110 @@ app.get("/api/games/:id", async (req, res, next) => {
   try {
     const game = await getGame(req.params.id, req.user);
     if (game) {
-      res.json(game)
+      res.json(game);
     } else {
       res.sendStatus(404);
     }
   } catch (e) {
-    next(e) 
+    next(e);
   }
 });
 
 app.get("/api/games", async (req, res, next) => {
   try {
-    const { sortBy, sortDescending, page, category, designer, mechanic } = req.query;
-    const game = await getGames({ 
-        sortBy, 
-        sortDescending, 
+    const {
+      sortBy,
+      sortDescending,
+      page,
+      category,
+      designer,
+      mechanic
+    } = req.query;
+    const game = await getGames(
+      {
+        sortBy,
+        sortDescending,
         page: page && toNumber(page),
-        category, 
-        designer, 
-        mechanic 
-      }, 
-      req.user, 
+        category,
+        designer,
+        mechanic
+      },
+      req.user
     );
     if (game) {
-      res.json(game)
+      res.json(game);
     } else {
       res.sendStatus(404);
     }
   } catch (e) {
-    next(e) 
+    next(e);
   }
 });
 
 app.get("/api/reviews", async (req, res, next) => {
   try {
     const { id, gameId, reviewerId, allowFlagged } = req.query;
-    const reviews = await getReviews({ id, gameId, reviewerId, allowFlagged }, req.user);
-    res.json(reviews)
+    const reviews = await getReviews(
+      { id, gameId, reviewerId, allowFlagged },
+      req.user
+    );
+    res.json(reviews);
   } catch (e) {
-    next(e) 
+    next(e);
   }
 });
 
 app.get("/api/flags/reviews", async (req, res, next) => {
   try {
     const flags = await getReviewFlags(req.user);
-    res.json(flags)
+    res.json(flags);
   } catch (e) {
-    next(e) 
+    next(e);
   }
 });
 
 app.post("/api/flags/reviews/:reviewId", async (req, res, next) => {
   try {
     await flagReview(req.params.reviewId, req.user);
-    res.sendStatus(200)
+    res.sendStatus(200);
   } catch (e) {
-    next(e) 
+    next(e);
   }
 });
 
 app.delete("/api/flags/reviews/:reviewId", async (req, res, next) => {
   try {
     await deleteReviewFlag(req.params.reviewId, req.user);
-    res.sendStatus(200)
+    res.sendStatus(200);
   } catch (e) {
-    next(e) 
+    next(e);
   }
 });
 
 app.get("/api/flags/reviewers", async (req, res, next) => {
   try {
     const flags = await getReviewerFlags(req.user);
-    res.json(flags)
+    res.json(flags);
   } catch (e) {
-    next(e) 
+    next(e);
   }
 });
 
 app.post("/api/flags/reviewers/:reviewerId", async (req, res, next) => {
   try {
     await flagReviewer(req.params.reviewerId, req.user);
-    res.sendStatus(200)
+    res.sendStatus(200);
   } catch (e) {
-    next(e) 
+    next(e);
   }
 });
 
 app.delete("/api/flags/reviewers/:reviewerId", async (req, res, next) => {
   try {
     await deleteReviewerFlag(req.params.reviewerId, req.user);
-    res.sendStatus(200)
+    res.sendStatus(200);
   } catch (e) {
-    next(e) 
+    next(e);
   }
 });
 
--- server/api/games.js
+++ server/api/games.js
@@ -1,25 +1,31 @@
-const db = require('../db/models/');
- 
+const db = require("../db/models/");
+
 const regulatingReviewScore = 5.5;
 const regulatingReviewCount = 200;
 const customParams = { regulatingReviewScore, regulatingReviewCount };
 
 module.exports = {};
 
-const allowedSortFields = ['name', 'play_time', 'min_age', 'bgg_rank', 'custom_rank', 'bgg_score', 'bgg_average', 'custom_avg', 'bgg_num_reviews', 'review_count']
+const allowedSortFields = [
+  "name",
+  "play_time",
+  "min_age",
+  "bgg_rank",
+  "custom_rank",
+  "bgg_score",
+  "bgg_average",
+  "custom_avg",
+  "bgg_num_reviews",
+  "review_count"
+];
 
-module.exports.getGame =  async function(id, userId) {
+module.exports.getGame = async function(id, userId) {
   const gameWithCustomRankingQuery = `
     SELECT *, game_rankings_${userId}.* FROM game
     JOIN game_rankings_${userId} ON game.id=game_rankings_${userId}."gameId"
     WHERE game.id=:id
   `;
-  const [
-    game,
-    mechanics,
-    categories,
-    designers
-  ] = await Promise.all([
+  const [game, mechanics, categories, designers] = await Promise.all([
     db.customQuery(gameWithCustomRankingQuery, { id, userId }).then(r => r[0]),
     db.customQuery(`SELECT * FROM game_mechanic WHERE "gameId"=:id`, { id }),
     db.customQuery(`SELECT * FROM game_category WHERE "gameId"=:id`, { id }),
@@ -31,30 +37,51 @@ module.exports.getGame =  async function(id, userId) {
     game.designers = designers.map(r => r.designer);
   }
   return game;
-}
+};
 
-
-module.exports.getGames =  async function({ sortBy = 'bgg_rank', sortDescending = false, page=0, category, designer, mechanic }, userId) {
+module.exports.getGames = async function(
+  {
+    sortBy = "bgg_rank",
+    sortDescending = false,
+    page = 0,
+    category,
+    designer,
+    mechanic
+  },
+  userId
+) {
   if (!allowedSortFields.includes(sortBy)) {
     throw Error("Sort By field not allowed");
   }
-  const joinField = category ? 'category' : (mechanic ? 'mechanic' : (designer ? 'designer' : null));
+  const joinField = category
+    ? "category"
+    : mechanic
+    ? "mechanic"
+    : designer
+    ? "designer"
+    : null;
   const query = `
     SELECT 
-      ${allowedSortFields.join(', ')}, thumbnail, game.id
+      ${allowedSortFields.join(", ")}, thumbnail, game.id
     FROM game
       JOIN game_rankings_${userId} ON game.id=game_rankings_${userId}."gameId"
-      ${ joinField ? `JOIN game_${joinField} on game.id=game_${joinField}."gameId" WHERE ${joinField}=:${joinField}` : '' }
-      ORDER BY ${sortBy} ${ sortDescending === 'true' ? 'DESC' : 'ASC' } NULLS LAST, bgg_rank DESC NULLS LAST
+      ${
+        joinField
+          ? `JOIN game_${joinField} on game.id=game_${joinField}."gameId" WHERE ${joinField}=:${joinField}`
+          : ""
+      }
+      ORDER BY ${sortBy} ${
+    sortDescending === "true" ? "DESC" : "ASC"
+  } NULLS LAST, bgg_rank DESC NULLS LAST
     LIMIT 10
     OFFSET :page*10
   `;
   return db.customQuery(query, { page, category, mechanic, designer, sortBy });
-}
+};
 
 module.exports.createView = function(userId) {
   const statement = `
-    CREATE MATERIALIZED VIEW IF NOT EXISTS game_rankings_${userId} AS (
+    CREATE MATERIALIZED VIEW game_rankings_${userId} AS (
       WITH scores AS (
         SELECT 
           "gameId", 
@@ -71,4 +98,4 @@ module.exports.createView = function(userId) {
     CREATE INDEX IF NOT EXISTS game_rankings_${userId}_pkey ON game_rankings_${userId} ("gameId");
   `;
   return db.customQuery(statement, { userId, ...customParams });
-}
+};
